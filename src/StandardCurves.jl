f5ea3315e3a5ad61d1496fd7c1afce7b9751b8df
Nick Stone <nicholasestone@proton.me>
2025-09-08 14:59:57 -0500

    Updated for arbitrary polynomial fits

--- src/StandardCurves.jl
+++ src/StandardCurves.jl
@@ -2,7 +2,7 @@ module StandardCurves
 
 using LinearAlgebra, RecipesBase
 
-export StandardCurve, LinearStandard
+export StandardCurve, LinearStandard, PolynomialStandard
 
 """
 Abstract type representing a standard curve. The curve can be applied to
@@ -15,27 +15,40 @@ abstract type StandardCurve end
 
 """
 ```julia
+PolynomialStandard{k}(X,Y)
+```
+curve of the form ``y_i = sum_k(b_k*x_i^k)``
+"""
+struct PolynomialStandard{k} <: StandardCurve
+    b_vec #weights corresponding of powers of x from 0 to k
+    function PolynomialStandard{k}(X::Vector{<:Number},Y::Vector{<:Number}) where k
+        @assert (k isa Int) "Degree of polynomial standard must be an Int"
+        #set up problem a la: https://en.wikipedia.org/wiki/Polynomial_regression#Matrix_form_and_calculation_of_estimates
+        A = [x^j for x in X, j in 0:k]
+        b = pinv(A) * Y
+        new(b)
+    end
+end
+
+function transform(ps::PolynomialStandard{k},x) where k
+    sum(enumerate(ps.b_vec)) do (i,bi)
+        bi*x^(i-1) #the first term is x^0, not x^1
+    end
+end
+
+"""
+```julia
 LinearStandard(X,Y)
 ```
 curve of the form ``y_i = m*x_i + b``
 """
-struct LinearStandard <: StandardCurve
-    m
-    b
-    function LinearStandard(X::Vector{<:Number},Y::Vector{<:Number})
-        xmat = hcat(X,ones(eltype(X),length(X)))
-        (m,b) = pinv(xmat) * Y
-        new(m,b)
-    end
-end
+const LinearStandard = PolynomialStandard{1}
 
-transform(ls::LinearStandard,x) = ls.m*x + ls.b
-
-@recipe function f(ls::LinearStandard,X,Y)
+@recipe function f(ls::PolynomialStandard,X,Y)
     @series begin
         seriestype := :path
         label := "fit"
-        xrange = [maximum(X),minimum(X)]
+        xrange = range(start=minimum(X), length=100, stop=maximum(X))
         (xrange,ls.(xrange))
     end
     seriestype := :scatter
